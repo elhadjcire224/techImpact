be4bb96c57a90e80fd3e44bc4f78ec81103da02c
elhadjcire224 <bahmamadoucire14@gmail.com>
2024-12-22 03:55:42 +0000

    feat: configure Google authentication with client ID, client secret, and redirect URI

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -8,13 +8,14 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     authorization: {
       params: {
+        redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
         prompt: "consent",
-        access_type: "offline",
-        response_type: "code"
       }
-    },
+    }
   })],
   adapter: PrismaAdapter(prisma),
   callbacks: {
